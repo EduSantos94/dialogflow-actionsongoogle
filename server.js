bf3d3c4f113e1d2f4a89e91050af7e41efedd304
Eduardo <38993997+EduSantos94@users.noreply.github.com>
2019-07-29 16:19:59 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,72 @@
+const {
+  dialogflow,
+  SignIn,
+  SimpleResponse
+} = require('actions-on-google')
+const express = require('express');
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
+const app = dialogflow({ clientId : process.env.CLIENT_ID })
+
+/*
+* Intent using Google Assistant Sign In
+*/
+app.intent('ask_for_sign_in_confirmation',(conv, params, signin) => {
+  
+  if (signin.status !== 'OK') return conv.ask(new SignIn());
+  var email = conv.user.email;
+  
+  return conv.ask(`Obrigado por logar, seu email é '${email}`);
+});
+
+/*
+* Integration with mysql to return user's name
+*/
+app.intent('pesquisar.alunos', (conv, params, signin) => {
+  
+  const aluno = params.aluno
+  
+  let resposta = pesquisar_aluno(aluno).then(function(results) {
+    
+    if (typeof results[0] === 'undefined') {
+      return `Registro não encontrado, verifique por erros ortograficos`
+    } else {
+      return results[0].nome
+    }
+  }).catch((err) => setImmediate(() => { throw err; }))
+  
+  return resposta.then(function(result){
+    conv.ask(result)
+  })
+})
+
+const expressApp = express().use(bodyParser.json())
+expressApp.post('process.env.URL', app)
+expressApp.listen(process.env.PORT)
+
+async function pesquisar_aluno(aluno)
+{
+  return new Promise(function (resolve,reject)
+  {
+    var connection = mysql_connection()
+    connection.connect()
+    
+    var query = `SELECT * FROM aluno WHERE id_aluno = "${aluno}"`
+    
+    connection.query(query, function (error, results, fields) 
+    {
+      if (error) return reject(error)
+      resolve(results)
+    })
+  })
+}
+
+function mysql_connection()
+{
+  return mysql.createConnection({
+    host : process.env.MYSQL_HOST,
+    user : process.env.MYSQL_USER,
+    password : process.env.MYSQL_PASS,
+    database : process.env.MYSQL_DB
+  })
+}
