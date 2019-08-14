ac8683108dab841e5514bb2534b5575c372ae412
Eduardo <38993997+EduSantos94@users.noreply.github.com>
2019-08-14 15:19:06 -0300

    Update model.js

--- model.js
+++ model.js
@@ -2,7 +2,10 @@
  * @author Eduardo dos Santos <eduardo.santos@edu.unipar.br>
  * Model destinado para todas as interações com o banco
  */
+
 const mysql = require('mysql')
+const Helper = require('./helper/helper_model')
+let helper = new Helper()
 
 module.exports = 
 {
@@ -13,7 +16,7 @@ module.exports =
       var connection = mysql_connection()
       connection.connect()
     
-      connection.query(pesquisar_aluno_query(email), function (error, results, fields) 
+      connection.query(helper.pesquisar_aluno_query(), email, function (error, results, fields) 
       {
         if (error) return reject(error)
         connection.end()
@@ -22,14 +25,14 @@ module.exports =
     })
   },
   
-  notas_alunos: async function(materia,email)
+  notas_alunos: async function(email,materia)
   {
     return new Promise(function (resolve, reject)
     {                  
       var connection = mysql_connection()
       connection.connect()
     
-      connection.query(notas_aluno_query(materia,email), function (error, results, fields) 
+      connection.query(helper.notas_aluno_query(), [email,materia], function (error, results, fields) 
       {
         if (error) return reject(error)
         connection.end()
@@ -45,7 +48,7 @@ module.exports =
       var connection = mysql_connection()
       connection.connect()
       
-      connection.query(lista_materia_query(email), function (error, results, fields) 
+      connection.query(helper.lista_materia_query(), email, function (error, results, fields) 
       {
         if (error) return reject(error)
         connection.end()
@@ -61,7 +64,23 @@ module.exports =
       var connection = mysql_connection()
       connection.connect()
 
-      connection.query(aulas_alunos_query(dia, email), function (error, results, fields)
+      connection.query(helper.aulas_alunos_query(), [dia, email], function (error, results, fields)
+      {
+        if (error) return reject(error)
+        connection.end()
+        resolve(results)
+      })
+    })
+  },
+  
+  notas_restante: async function(materia, email)
+  {
+    return new Promise(function (resolve, reject)
+    {
+      var connection = mysql_connection()
+      connection.connect()
+      console.log(email)
+      connection.query(helper.notas_restante_query(), [email, materia], function (error, results, fields)
       {
         if (error) return reject(error)
         connection.end()
@@ -79,37 +98,4 @@ function mysql_connection()
     password : process.env.MYSQL_PASS,
     database : process.env.MYSQL_DB
   })
-}
-
-function pesquisar_aluno_query(email)
-{
-  return `SELECT * FROM aluno WHERE email = "${email}"`
-}
-
-function notas_aluno_query(materia,email)
-{
-  return `SELECT n.nota as nota, m.nome as nome 
-      FROM notas AS n 
-      INNER JOIN materia AS m ON m.id_materia = n.id_materia
-      INNER JOIN aluno AS a ON a.id_aluno = n.id_aluno
-      WHERE a.email = "${email}"
-      AND m.nome = "${materia}" ORDER BY bimestre DESC`
-}
-
-function lista_materia_query(email)
-{
-  return `SELECT m.nome 
-      FROM materia as m 
-      INNER JOIN curso_materia as cm ON cm.id_materia = m.id_materia 
-      WHERE cm.id_curso = (SELECT a.id_curso FROM aluno as a WHERE a.email = "${email}")`
-}
-
-function aulas_alunos_query(dia, email)
-{
-  return `SELECT  m.nome as nome, a.horario as horario
-      FROM aluno_aula as aa INNER JOIN aula as a ON a.id_aula = aa.id_aula
-      INNER JOIN aluno AS al ON al.id_aluno = aa.id_aluno
-      INNER JOIN materia AS m ON a.id_materia = m.id_materia
-      WHERE a.dia = "${dia}"
-      AND al.email = "${email}"`
 }
