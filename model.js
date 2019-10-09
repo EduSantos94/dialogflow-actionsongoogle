a39bc0fe6dbab3118115f9ee322727889fea4a06
Eduardo <38993997+EduSantos94@users.noreply.github.com>
2019-10-09 07:27:57 -0300

    Adiciona opção do aluno ver nota por bimestre

--- model.js
+++ model.js
@@ -3,99 +3,102 @@
  * Model destinado para todas as interações com o banco
  */
 
-const mysql = require('mysql')
-const Helper = require('./helper/helper_model')
-let helper = new Helper()
+const mysql = require("mysql");
+const Helper = require("./helper/helper_model");
+let helper = new Helper();
 
-module.exports = 
-{
-  pesquisar_aluno: async function(email)
-  {
-    return new Promise(function (resolve,reject)
-    {
-      var connection = mysql_connection()
-      connection.connect()
-    
-      connection.query(helper.pesquisar_aluno_query(), email, function (error, results, fields) 
-      {
-        if (error) return reject(error)
-        connection.end()
-        resolve(results)
-      })
-    })
-  },
-  
-  notas_alunos: async function(email,materia)
-  {
-    return new Promise(function (resolve, reject)
-    {                  
-      var connection = mysql_connection()
-      connection.connect()
-    
-      connection.query(helper.notas_aluno_query(), [email,materia], function (error, results, fields) 
-      {
-        if (error) return reject(error)
-        connection.end()
-        resolve(results)
-      })
-    })
-  },
-  
-  lista_materia: async function(email)
-  {
-    return new Promise(function (resolve, reject)
-    {
-      var connection = mysql_connection()
-      connection.connect()
-      
-      connection.query(helper.lista_materia_query(), email, function (error, results, fields) 
-      {
-        if (error) return reject(error)
-        connection.end()
-        resolve(results)
-      })
-    })
-  },
-  
-  aulas_alunos: async function(dia, email)
-  {
-    return new Promise(function (resolve, reject)
-    {
-      var connection = mysql_connection()
-      connection.connect()
+module.exports = {
+  pesquisar_aluno: async function(email) {
+    return new Promise(function(resolve, reject) {
+      var connection = mysql_connection();
+      connection.connect();
 
-      connection.query(helper.aulas_alunos_query(), [dia, email], function (error, results, fields)
-      {
-        if (error) return reject(error)
-        connection.end()
-        resolve(results)
-      })
-    })
+      connection.query(helper.pesquisar_aluno_query(), email, function(
+        error,
+        results,
+        fields
+      ) {
+        if (error) return reject(error);
+        connection.end();
+        resolve(results);
+      });
+    });
   },
-  
-  notas_restante: async function(materia, email)
-  {
-    return new Promise(function (resolve, reject)
-    {
-      var connection = mysql_connection()
-      connection.connect()
-      console.log(email)
-      connection.query(helper.notas_restante_query(), [email, materia], function (error, results, fields)
-      {
-        if (error) return reject(error)
-        connection.end()
-        resolve(results)
-      })
-    })
+
+  notas_alunos: async function(email, materia, bimestre) {
+    return new Promise(function(resolve, reject) {
+      var connection = mysql_connection();
+      connection.connect();
+
+      connection.query(
+        helper.notas_aluno_query(),
+        [email, materia, bimestre],
+        function(error, results, fields) {
+          if (error) return reject(error);
+          connection.end();
+          resolve(results);
+        }
+      );
+    });
+  },
+
+  lista_materia: async function(email) {
+    return new Promise(function(resolve, reject) {
+      var connection = mysql_connection();
+      connection.connect();
+
+      connection.query(helper.lista_materia_query(), email, function(
+        error,
+        results,
+        fields
+      ) {
+        if (error) return reject(error);
+        connection.end();
+        resolve(results);
+      });
+    });
+  },
+
+  aulas_alunos: async function(dia, email) {
+    return new Promise(function(resolve, reject) {
+      var connection = mysql_connection();
+      connection.connect();
+
+      connection.query(helper.aulas_alunos_query(), [dia, email], function(
+        error,
+        results,
+        fields
+      ) {
+        if (error) return reject(error);
+        connection.end();
+        resolve(results);
+      });
+    });
+  },
+
+  notas_restante: async function(materia, email) {
+    return new Promise(function(resolve, reject) {
+      var connection = mysql_connection();
+      connection.connect();
+      console.log(email);
+      connection.query(
+        helper.notas_restante_query(),
+        [email, materia],
+        function(error, results, fields) {
+          if (error) return reject(error);
+          connection.end();
+          resolve(results);
+        }
+      );
+    });
   }
-}
+};
 
-function mysql_connection()
-{
+function mysql_connection() {
   return mysql.createConnection({
-    host : process.env.MYSQL_HOST,
-    user : process.env.MYSQL_USER,
-    password : process.env.MYSQL_PASS,
-    database : process.env.MYSQL_DB
-  })
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DB
+  });
 }
